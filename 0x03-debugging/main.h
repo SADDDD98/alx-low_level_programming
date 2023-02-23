cf27f202442b72e97c0a6b125fa05af7fe7e5f11
SADDDD98 <tarik.el.fallahi98@gmail.com>
2023-02-23 11:51:57 -0800

    1.like, comment, subscribe

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,3 @@
-main.h
-
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
@@ -10,4 +8,3 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif
-
